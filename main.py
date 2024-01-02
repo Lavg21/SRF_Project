111e91ee1ed0d2a916b1cb03981865bbdce8ef62
Lavg21 <laviniag336@gmail.com>
2024-01-02 15:49:20 +0200

    Created test folder

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
+# import os
+# import json
+# import shutil
+#
+# def assign_categories_and_move(folder_path, json_path, output_folder):
+#     # Citirea informațiilor din fișierul JSON
+#     with open(json_path, 'r') as json_file:
+#         data = json.load(json_file)
+#
+#     # Crearea unui dicționar pentru a asocia id-urile de imagini cu categoriile date
+#     image_categories = {img['id']: data['categories'][ann['category_id']]['name']
+#                         for img in data.get('images', [])
+#                         for ann in data.get('annotations', []) if ann['image_id'] == img['id']}
+#
+#     # Crearea unor subfoldere corespunzătoare categoriilor date
+#     for category in set(image_categories.values()):
+#         category_folder = os.path.join(output_folder, category)
+#         os.makedirs(category_folder, exist_ok=True)
+#
+#     # Mutarea imaginilor în subfolderele corespunzătoare
+#     for image in data.get('images', []):
+#         image_id = image['id']
+#         filename = image['file_name']
+#         filepath = os.path.join(folder_path, filename)
+#
+#         # Verificare dacă fișierul este imagine și este prezent în dicționarul de categorii
+#         if os.path.isfile(filepath) and image_id in image_categories:
+#             category = image_categories[image_id]
+#             destination_folder = os.path.join(output_folder, category)
+#             destination_path = os.path.join(destination_folder, filename)
+#
+#             # Mutarea fișierului în subfolderul corespunzător
+#             shutil.move(filepath, destination_path)
+#             print(f'{filename}: {category}')
+#
+# if __name__ == "__main__":
+#     folder_path = r"D:\Facultate\UTCN\An IV\Semestrul 1\Sisteme de recunoastere a formelor\Proiect\script\train"
+#     json_path = r"D:\Facultate\UTCN\An IV\Semestrul 1\Sisteme de recunoastere a formelor\Proiect\script\train\_annotations.coco.json"
+#     output_folder = r"D:\Facultate\UTCN\An IV\Semestrul 1\Sisteme de recunoastere a formelor\Proiect\script\output_folder"
+#     assign_categories_and_move(folder_path, json_path, output_folder)
+
 import os
 import json
 import shutil
@@ -34,7 +75,7 @@ def assign_categories_and_move(folder_path, json_path, output_folder):
             print(f'{filename}: {category}')
 
 if __name__ == "__main__":
-    folder_path = r"D:\Facultate\UTCN\An IV\Semestrul 1\Sisteme de recunoastere a formelor\Proiect\script\train"
-    json_path = r"D:\Facultate\UTCN\An IV\Semestrul 1\Sisteme de recunoastere a formelor\Proiect\script\train\_annotations.coco.json"
-    output_folder = r"D:\Facultate\UTCN\An IV\Semestrul 1\Sisteme de recunoastere a formelor\Proiect\script\output_folder"
+    folder_path = r"D:\Facultate\UTCN\An IV\Semestrul 1\Sisteme de recunoastere a formelor\Proiect\script\valid"
+    json_path = r"D:\Facultate\UTCN\An IV\Semestrul 1\Sisteme de recunoastere a formelor\Proiect\script\valid\_annotations.coco.json"
+    output_folder = r"D:\Facultate\UTCN\An IV\Semestrul 1\Sisteme de recunoastere a formelor\Proiect\script\output_folder_test"
     assign_categories_and_move(folder_path, json_path, output_folder)
